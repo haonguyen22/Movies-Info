9553c88c66fccfabd61b1c21f35ff7ae508de114
20120470 <haonguyencp22@gmail.com>
2022-10-22 22:17:57 +0700

    hover item

--- js/ListFilmScroll.js
+++ js/ListFilmScroll.js
@@ -11,12 +11,27 @@ export default {
                <div class="film__scroll">
                     <button>&lt;</button>
                     <div class="film__list">
-                         <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
-                              alt="The Shawshank Redemption">
-                         <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
-                              alt="The Shawshank Redemption">
-                         <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
-                              alt="The Shawshank Redemption">
+                         <div class="film__item">
+                              <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
+                                   alt="The Shawshank Redemption">
+                              <div class="film__name">
+                                   <h4>The Shawshank Redemption (1994)</h4>
+                              </div>
+                         </div>
+                         <div class="film__item">
+                              <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
+                                   alt="The Shawshank Redemption">
+                              <div class="film__name">
+                                   <h4>The Shawshank Redemption (1994)</h4>
+                              </div>
+                         </div>
+                         <div class="film__item">
+                              <img src="https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,12,128,176_AL_.jpg"
+                                   alt="The Shawshank Redemption">
+                              <div class="film__name">
+                                   <h4>The Shawshank Redemption (1994)</h4>
+                              </div>
+                         </div>
                     </div>
                     <button>&gt;</button>
                </div>
